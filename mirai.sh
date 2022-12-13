4a59405350af085c5c7e1ca9219ed6c29901e56d
Liu Jun <me@zinglix.xyz>
2022-12-13 22:16:15 +0800

    Update mirai.sh

--- mirai.sh
+++ mirai.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! -f ./mcl ]; then
-    wget https://github.com/iTXTech/mirai-console-loader/releases/download/v1.0.5/mcl-1.0.5.zip -O mcl.zip
-    unzip mcl.zip
+    wget https://github.com/iTXTech/mirai-console-loader/releases/download/v2.1.2/mcl-2.1.2.zip
+    unzip mcl-2.1.2.zip
     rm mcl.zip 
     chmod +x ./mcl
     ./mcl --update-package net.mamoe:mirai-api-http --channel stable --dry-run
